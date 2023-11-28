ee88f1b2103d20cfbf1a7a4c07e53a2716db1414
Win Nandar Khin <winnandarkhin@gmail.com>
2023-11-28 12:05:34 +0630

    Add Project Structure

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Typography variant="h1">Welcome to my SIMPLE WEB CHECK.</Typography>
+    </main>
+  )
+}
